216906ad3b37f77bc6e22b43b0b7eed63a5e2e8a
thaihuyen <huyennguyen230103@gmail.com>
2023-07-15 10:50:34 +0700

    add product images

--- main.js
+++ main.js
@@ -109,4 +109,53 @@ $(".slider_area").owlCarousel({
 });
 
 
+
+//product
+$(".product_column4")
+    .on("changed.owl.carousel initialized.owl.carousel", function (event) {
+      $(event.target)
+        .find(".owl-item")
+        .removeClass("last")
+        .eq(event.item.index + event.page.size - 1)
+        .addClass("last");
+    })
+    .owlCarousel({
+      autoplay: true,
+      loop: true,
+      nav: true,
+      autoplay: false,
+      autoplayTimeout: 5000,
+      items: 4,
+      dots: false,
+      navText: [
+        '<i class="fa fa-arrow-left"></i>',
+        '<i class="fa fa-arrow-right"></i>',
+      ],
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        576: {
+          items: 2,
+        },
+        768: {
+          items: 3,
+        },
+        992: {
+          items: 4,
+        },
+      },
+    });
+
+
+//tooltip activation
+$(".action_links ul li a, .add_to_cart a, .footer_social_links ul li a").tooltip({
+  animated: "fade",
+  placement: "top",
+  container: "body",
+})
+
+
+
 })(jQuery);
